6fdd6b33000c9dab90e92d03e3d510c73c3b65eb
Lazyfella <alexander.tallgren@gmail.com>
2022-11-19 11:39:59 +0200

    Pathfinding 4 connection to target
    Gridbased pathfinding, find the target but not movement yet.

---
+++ Assets/_Scripts/PathFinding/BlockerTest.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using Pathfinding;
+public class BlockerTest : MonoBehaviour {
+    public void Start () {
+        var blocker = GetComponent<SingleNodeBlocker>();
+        blocker.BlockAtCurrentPosition();
+    }
+}
