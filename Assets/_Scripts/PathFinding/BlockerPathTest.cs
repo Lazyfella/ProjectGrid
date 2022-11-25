dbded38d8a7e222133496c45dc0ebdbeb54a6ff8
Lazyfella <alexander.tallgren@gmail.com>
2022-11-25 20:26:36 +0200

    Added movement to character
    Moves toward target. Need to figure out how to snap to the A* path.

--- Assets/_Scripts/PathFinding/BlockerPathTest.cs
+++ Assets/_Scripts/PathFinding/BlockerPathTest.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 using System.Collections.Generic;
 using Pathfinding;
 
-public class BlockerPathTest : MonoBehaviour {
+public class BlockerPathTest : MonoBehaviour
+{
+    public int movementPoints = 2;
     public BlockManager blockManager;
     public List<SingleNodeBlocker> obstacles;
     public Transform target;
 
     BlockManager.TraversalProvider traversalProvider;
 
-    public void Start () {
+    public void Start()
+    {
         // Create a traversal provider which says that a path should be blocked by all the SingleNodeBlockers in the obstacles array
         traversalProvider = new BlockManager.TraversalProvider(blockManager, BlockManager.BlockMode.OnlySelector, obstacles);
     }
 
-    public void Update () {
+    public void Update()
+    {
         // Create a new Path object
         var path = ABPath.Construct(transform.position, target.position, null);
 
@@ -24,13 +28,20 @@ public class BlockerPathTest : MonoBehaviour {
         // Calculate the path synchronously
         AstarPath.StartPath(path);
         path.BlockUntilCalculated();
-        
-        if (path.error) {
+        //Moves towards target (NEEDS TO FOLLOW PATH)
+        transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * movementPoints);
+
+        if (path.error)
+        {
             Debug.Log("No path was found");
-        } else {
+        }
+        else
+        {
             Debug.Log("A path was found with " + path.vectorPath.Count + " nodes");
+
             // Draw the path in the scene view
-            for (int i = 0; i < path.vectorPath.Count - 1; i++) {
+            for (int i = 0; i < path.vectorPath.Count - 1; i++)
+            {
                 Debug.DrawLine(path.vectorPath[i], path.vectorPath[i + 1], Color.red);
             }
         }
